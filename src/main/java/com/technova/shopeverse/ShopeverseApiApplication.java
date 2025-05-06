242685c604a3ffcc9bf92ae3e0b235bff869e33a
Marcela  Arratia Fuentes <marratiaf.accenture+bch@bancochile.cl>
2025-05-06 19:28:08 -0400

    first commit

---
+++ src/main/java/com/technova/shopeverse/ShopeverseApiApplication.java
@@ -0,0 +1,13 @@
+package com.technova.shopeverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeverseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeverseApiApplication.class, args);
+	}
+
+}
